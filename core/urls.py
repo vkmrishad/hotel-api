5b95ad7d431accfd004815d85714053189cce1ef
Mohammed Rishad <mohammedrishad.vk@gmail.com>
2025-05-29 19:37:54 +0530

    Initial Django Setup * Added Pre-commit hook * uv - For package dependency and virtual env management * Added Ruff for Styling, Formatting and import sorting * Added Swagger/OpenAPI for Docs

---
+++ core/urls.py
@@ -0,0 +1,54 @@
+"""
+URL configuration for core project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+base_url = "api"
+
+urlpatterns = (
+    [
+        path("", lambda request: redirect("admin:index")),  # Redirect root to admin
+        path("admin/", admin.site.urls),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
+
+# Swagger
+if settings.DEBUG:
+    urlpatterns += [
+        path("schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/swagger/",
+            SpectacularSwaggerView.as_view(
+                url="/schema/?format=json", url_name="schema"
+            ),
+            name="swagger-ui",
+        ),
+        path(
+            "api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+        ),
+    ]
